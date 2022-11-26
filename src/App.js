aad4a552cfd3e2abfdee88e9adf0217aebb748ff
naim71 <naimkhan1314@gmail.com>
2022-11-27 05:43:25 +0600

    toast

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Router';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="max-w-[1440px] mx-auto bg-primary mt-5">
       <RouterProvider router={router}></RouterProvider>
+      <Toaster
+      position="top-right"
+      reverseOrder={false}
+      ></Toaster>
     </div>
   );
 }
--- src/Pages/Products/Modal/Modal.js
+++ src/Pages/Products/Modal/Modal.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const Modal = ({ book, user, setBook }) => {
-    const {displayName, email} = user;
+    const { displayName, email } = user;
     const { name, price } = book;
 
-    const handleBooking = event =>{
+    const handleBooking = event => {
         event.preventDefault();
         const form = event.target;
         const username = form.username.value;
@@ -13,7 +14,7 @@ const Modal = ({ book, user, setBook }) => {
         const product_price = form.price.value;
         const number = form.number.value;
         const meeting_place = form.meeting_place.value;
-        
+
         const booking = {
             username,
             email: emailaddress,
@@ -23,7 +24,25 @@ const Modal = ({ book, user, setBook }) => {
             meetingPlace: meeting_place,
         }
         console.log(booking);
-        setBook(null);
+
+        //sending data to server
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.acknowledged){
+                    setBook(null);
+                    toast.success('Successfully Booked!')
+
+                }
+
+            })
 
     }
 
@@ -39,28 +58,28 @@ const Modal = ({ book, user, setBook }) => {
                             <h3 className="text-lg font-bold">Product: {name}</h3>
 
                             <form onSubmit={handleBooking} action="" className='mt-5'>
-                                <input name="username"  defaultValue={user?.displayName} disabled type="text" placeholder="username" className="input input-bordered input-warning w-full input-sm mt-3" />
+                                <input name="username" defaultValue={user?.displayName} disabled type="text" placeholder="username" className="input input-bordered input-warning w-full input-sm mt-3" />
                                 <input name="email" defaultValue={user?.email} type="email" disabled placeholder="email address" className="input input-bordered input-warning w-full input-sm mt-3" />
                                 <input name="product_name" defaultValue={name} disabled type="text" placeholder="product name" className="input input-bordered input-warning w-full input-sm mt-3" />
                                 <input name="price" defaultValue={price} disabled type="number" placeholder="120" className="input input-bordered input-warning w-full input-sm mt-3" />
-                                <input name="number" type="number" placeholder="phone number" className="input input-bordered input-warning w-full input-sm mt-3" required/>
-                                <input name="meeting_place" type="text" placeholder="meeting location" className="input input-bordered input-warning w-full input-sm mt-3" required/>
+                                <input name="number" type="number" placeholder="phone number" className="input input-bordered input-warning w-full input-sm mt-3" required />
+                                <input name="meeting_place" type="text" placeholder="meeting location" className="input input-bordered input-warning w-full input-sm mt-3" required />
                                 <input type="submit" className="btn w-full input-sm mt-5 btn-primary rounded-md" value="Submit" />
                             </form>
                         </div>
                     </div></>
-                    :
-                    <>
+                :
+                <>
                     <input type="checkbox" id="product-modal" className="modal-toggle" />
                     <div className="modal">
                         <div className="modal-box relative">
                             <label htmlFor="product-modal" className="btn btn-sm btn-primary btn-circle absolute right-2 top-2">✕</label>
 
-                            </div>
+                        </div>
                     </div>
-                    </>
+                </>
 
-        }
+            }
         </>
     );
 };
