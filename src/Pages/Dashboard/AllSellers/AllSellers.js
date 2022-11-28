5271d10c48718c76dd19b05a92b94388aa2156f0
naim71 <naimkhan1314@gmail.com>
2022-11-28 21:28:59 +0600

    delete user

--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,37 +1,58 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import toast from 'react-hot-toast';
+import ConfirmationModal from '../../../components/ConfirmationModal/ConfirmationModal';
 
 const AllSellers = () => {
-    const {data: users = [], refetch} = useQuery({
+    const [deletingSeller, setDeletingSeller] = useState(null);
+
+    const closeModal = () => {
+        setDeletingSeller(null);
+    }
+
+
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
-        queryFn: async() =>{
+        queryFn: async () => {
             const res = await fetch('http://localhost:5000/users?role=Seller');
             const data = await res.json();
             return data;
         }
     });
 
-    const handleVerify = (id, name) =>{
-        fetch(`http://localhost:5000/users/${id}`,{
+    const handleVerify = (id, name) => {
+        fetch(`http://localhost:5000/users/${id}`, {
             method: 'PUT'
         })
-        .then(res => res.json())
-        .then(data => {
-            if(data.modifiedCount > 0){
-                toast.success('Seller Verified!😃');
-                refetch();
-            }
-        })
-
-        fetch(`http://localhost:5000/products/${name}`,{
-            method: 'PUT'
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('Seller Verified!😃');
+                    refetch();
+                }
             })
 
+        fetch(`http://localhost:5000/products/${name}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+    }
+
+    const handleDeleteSeller = user => {
+        fetch(`http://localhost:5000/users/seller/${user.email}`, {
+            method: 'DELETE', 
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                refetch();
+                toast.success(`${user.name} deleted successfully`)
+            }
+        })
     }
 
 
@@ -53,31 +74,42 @@ const AllSellers = () => {
                     <tbody>
 
                         {
-                            users.map((user,i)=><tr key={i}>
-                            <th>{i+1}</th>
-                            <td>
-                              {user.name}
-                            </td>
-                            <td>
-                                {user.email}
-                            </td>
-                            <td>{user.role}</td>
-                            <th>
-                                <button className="btn bg-red-600 border-none text-white btn-xs normal-case">Delete</button>
-                            </th>
-                            <th>
-                            { user?.status !== 'verified' && <button onClick={()=>handleVerify(user._id, user.name)} className="btn bg-blue-500 border-none text-white btn-xs normal-case">Verify Seller</button>}
-                            </th>
-                        </tr>)
+                            users.map((user, i) => <tr key={i}>
+                                <th>{i + 1}</th>
+                                <td>
+                                    {user.name}
+                                </td>
+                                <td>
+                                    {user.email}
+                                </td>
+                                <td>{user.role}</td>
+                                <th>
+                                    <label onClick={() => setDeletingSeller(user)} htmlFor="confirmation-modal" className="btn btn-sm bg-red-500 text-white">Delete</label>
+                                </th>
+                                <th>
+                                    {user?.status !== 'verified' && <button onClick={() => handleVerify(user._id, user.name)} className="btn bg-blue-500 border-none text-white btn-xs normal-case">Verify Seller</button>}
+                                </th>
+                            </tr>)
                         }
-                        
+
                     </tbody>
-                  
+
 
                 </table>
             </div>
+            {
+                deletingSeller && <ConfirmationModal
+                    title={`Are you sure you want to delete?`}
+                    message={`Do you want to delete ${deletingSeller.name}? It cannot be undone.`}
+                    successAction={handleDeleteSeller}
+                    successButtonName="Delete"
+                    modalData={deletingSeller}
+                    closeModal={closeModal}
+                >
+                </ConfirmationModal>
+            }
         </div>
     );
 };
 
-export default AllSellers;<p>Sellers</p>
+export default AllSellers; <p>Sellers</p>
